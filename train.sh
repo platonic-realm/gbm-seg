800c5e508de3bfbb87b8379eddf3cc692cdeeac4
Arash Fatehi <fatehi.arash@gmail.com>
2022-12-05 16:14:42 +0100

    debug/standalone mode added to train.sh

--- train.sh
+++ train.sh
@@ -5,24 +5,44 @@ set -o nounset  # fail if there is an unset variable
 set -o pipefail # helps to catch pip issues
 
 work_dir=$(dirname "$(readlink --canonicalize-existing "${0}" 2> /dev/null)")
+config_name=train
+no_of_nodes=1
+node_rank=0
+debug=false
+standalone=false
 
-if [ "$#" -ne 2 ]; then
-    echo "Default: configs=train, "
-    echo "Please provide configuration name and node's rank"
-    config_name=train
-    no_of_nodes=1
-    node_rank=0
-else
-    config_name=$1
-    node_rank=$2
-fi
+# For parsing arguments I am using getops, its not a robust way of
+# handling it, but it gets the job done and for the time being, 
+# I don't need anything fancy...
+# https://sookocheff.com/post/bash/parsing-bash-script-arguments-with-shopts/
+while getopts ":dsc:n:" opt; do
+    case ${opt} in
+        d )
+            echo "debuging..."
+            debug=true
+            ;;
+        s )
+            echo "runnig torchrun in standalone mode"
+            standalone=true
+            ;;
+        c )
+            echo "Setting configuration to $OPTARG"
+            config_name=$OPTARG
+            ;;
+        n )
+            echo "Setting node rank: $OPTARG"
+            node_rank=$OPTARG
+            ;;
+    esac
+done
+shift $((OPTIND -1))
 
 config_path=$work_dir/configs/$config_name.yaml
 configs=$(cat $config_path)
 
 script_path=$work_dir/train.py
 
-
+# Using python to parse yaml files and extract config values
 yaml() {
     python3 -c "import yaml;print(yaml.safe_load(open('$1'))$2)"
 }
@@ -35,12 +55,22 @@ rdzv_endpoint=$(yaml $config_path "['trainer']['ddp']['rdzv_endpoint']")
 
 result_dir=../results/$config_name-$model_name-$model_tag
 
-torchrun \
-    --nproc_per_node=gpu \
-    --nnodes=$no_of_nodes \
-    --node_rank=$node_rank \
-    --rdzv_id=643 \
-    --rdzv_backend=$rdzv_backend \
-    --rdzv_endpoint=$rdzv_endpoint \
-    $script_path -c $config_path -rp $result_dir
-
+if $debug
+then
+    python -m pudb $script_path -c $config_path -rp $result_dir
+elif $standalone
+then
+    torchrun \
+        --standalone \
+        --nproc_per_node=gpu \
+        $script_path -c $config_path -rp $result_dir
+else
+    torchrun \
+        --nproc_per_node=gpu \
+        --nnodes=$no_of_nodes \
+        --node_rank=$node_rank \
+        --rdzv_id=643 \
+        --rdzv_backend=$rdzv_backend \
+        --rdzv_endpoint=$rdzv_endpoint \
+        $script_path -c $config_path -rp $result_dir
+fi
